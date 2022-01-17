324198e0dfe57f1598361a5693796099c9e77859
Gachio <bellotaophyc@outlook.com>
2022-01-17 15:23:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.clear();
+
+let button = 
+ document.querySelector("button");
+let card = document.querySelector(".card");
+  button.addEventListener("click", () => {
+  card.classList.add("away");
+})
